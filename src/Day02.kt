44bc5e654c88469986321d13aa18baf799985d2c
Alexander Schmidt <schmidt@ponton.de>
2025-12-02 09:34:57 +0100

    day02

---
+++ src/Day02.kt
@@ -0,0 +1,41 @@
+fun main() {
+    fun parseIdRange(input: List<String>): List<LongRange> {
+        return input.map {
+            val (start, end) = it.split("-")
+            start.toLong()..end.toLong()
+        }
+    }
+
+    fun part1(input: List<String>): Long {
+        val ranges = parseIdRange(input.first().split(","))
+        return ranges.map { it.filter { it.invalidId() } }.flatten().sumOf { it }
+    }
+
+    fun part2(input: List<String>): Long {
+        val ranges = parseIdRange(input.first().split(","))
+        return ranges.map { it.filter { it.invalidId2() } }.flatten().sumOf { it }
+    }
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day02_test")
+    part1(testInput).println()
+    check(part1(testInput) == 1227775554L)
+    check(part2(testInput) == 4174379265L)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
+
+fun Long.invalidId(): Boolean = this.toString().let { s -> s.take(s.length / 2) == s.drop(s.length / 2) }
+fun Long.invalidId2(): Boolean {
+    val s = this.toString()
+    var index = 1
+    while (index <= s.length/2) {
+        val sub = s.take(index)
+        if (sub.repeat(s.length/sub.length) == s) return true
+        index++
+    }
+    return false
+}
