981d0dd7d26ae9bd96ddc73a1c4d9c5e29a1e123
Alexander Schmidt <schmidt@ponton.de>
2025-12-04 11:56:24 +0100

    day04

---
+++ src/Day04.kt
@@ -0,0 +1,54 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        val grid: Grid<Char> = input.toGrid { it }
+        var counter = 0
+        for (cell in grid.cells()) {
+            if (cell.value != '@') continue
+            if (grid.adjacentCells(cell).count { it.value == '@' } < 4) counter++
+        }
+        return counter
+    }
+
+    fun part2(input: List<String>): Int {
+        val grid: Grid<Char> = input.toGrid { it }
+        var counter = 0
+        val rollsToRemove = mutableListOf<Cell<Char>>()
+        var done = false
+        while (!done) {
+            for (cell in grid.cells()) {
+                if (cell.value != '@') continue
+                if (grid.adjacentCells(cell).count { it.value == '@' } < 4) {
+                    counter++
+                    rollsToRemove.add(cell)
+                }
+            }
+            if (rollsToRemove.isEmpty()) done = true
+            rollsToRemove.forEach { cell -> cell.value = '.' }
+            rollsToRemove.clear()
+        }
+        return counter
+    }
+
+    fun part2b(input: List<String>): Int {
+        val grid: Grid<Char> = input.toGrid { it }
+        return generateSequence {
+            grid.cells()
+                .filter { it.value == '@' && grid.adjacentCells(it).count { adj -> adj.value == '@' } < 4 }
+                .ifEmpty { null }
+        }.sumOf { cells ->
+            cells.forEach { cell -> cell.value = '.' }
+            cells.size
+        }
+    }
+
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 13)
+    check(part2(testInput) == 43)
+    check(part2b(testInput) == 43)
+
+    val input = readInput("Day04")
+    part1(input).println()
+    part2(input).println()
+    part2b(input).println()
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,30 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+typealias Grid<T> = Array<Array<Cell<T>>>
+
+fun <T> List<String>.toGrid(convert: (Char) -> T): Grid<T> =
+    mapIndexed { y, row -> row.toCharArray().mapIndexed { x, value -> Cell(convert(value), x, y) }.toTypedArray() }.toTypedArray()
+
+data class Cell<T>(var value: T, val x: Int, val y: Int)
+
+fun <T> Grid<T>.print() = forEach { println(it.joinToString { c -> c.value.toString() }) }
+
+fun <T> Grid<T>.adjacentCells(cell: Cell<T>): List<Cell<T>> {
+    return listOf(
+        cell.x - 1 to cell.y - 1,
+        cell.x - 1 to cell.y,
+        cell.x - 1 to cell.y + 1,
+        cell.x to cell.y - 1,
+        cell.x to cell.y + 1,
+        cell.x + 1 to cell.y - 1,
+        cell.x + 1 to cell.y,
+        cell.x + 1 to cell.y + 1
+    ).mapNotNull { runCatching { this[it.second][it.first] }.getOrNull() }
+}
+
+fun <T> Grid<T>.cells() = this.flatMap { it.toList() }
+
+inline fun <T> Grid<T>.forEachCell(action: (Cell<T>) -> Unit) =
+    cells().forEach { cell -> action(cell) }
